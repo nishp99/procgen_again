fb7881cdb2b1864362e66ce1c7caee9d96f6ab92
nshl99 <78479831+nshl99@users.noreply.github.com>
2023-06-26 18:17:26 +0100

    'ditto'

--- bossfight/train_eval.py
+++ bossfight/train_eval.py
@@ -5,7 +5,7 @@ import os
 def run(training_path, evaluation_path, penalty=0, alpha=1, beta=1e-6, max_episodes=500000, Nagents=10, eval_episodes=1000):
 	train_path = os.path.join(training_path, f'pen{penalty}')
 	#os.makedirs(train_path, exist_ok=True)
-	eval_path = os.path.join(evaluation_path, f'pen{penalty}')
+	eval_path = os.path.join(evaluation_path, f'pen{penalty}.npz')
 	#os.makedirs(evaluation_path, exist_ok=True)
 
 	train(train_path, penalty, alpha, max_episodes, Nagents)
