e6c242cd00475d67e0c98b18a577751b6a5cfc15
nshl99 <78479831+nshl99@users.noreply.github.com>
2023-06-26 18:10:18 +0100

    another path file ditto etc

--- bossfight/script.py
+++ bossfight/script.py
@@ -3,25 +3,33 @@ import os
 from train_eval import run
 import submitit
 
+
+
 #start timestamp with unique identifier for name
 run_timestamp = datetime.datetime.now().strftime('%Y%m-%d%H-%M%S')
 #os.mkdir(with name of unique identifier)
 
-#os.path.join(results, unique identifier)
-training_path = os.path.join(run_timestamp, "training_data")
-os.makedirs(results_path, exist_ok = True)
+results_folder = os.path.join('results', 'our_update')
 
-experiment_path = os.path.join(results_path, "outputs")
+#connect to results folder
+results_run = os.path.join(results_folder, run_timestamp)
+os.makedirs(results_run, exist_ok = True)
+
+#make data folder inside timestamp
+data = os.path.join(results_run, 'data')
+os.makedirs(data, exist_ok = True)
+
+#os.path.join(results, unique identifier)
+training_path = os.path.join(data, "training_data")
+os.makedirs(training_path, exist_ok = True)
+
+evaluation_path = os.path.join(data, "evaluation_data")
+os.makedirs(evaluation_path, exist_ok = True)
+
+outputpath = os.path.join(results_run, "outputs")
 os.makedirs(experiment_path, exist_ok = True)
 
-evaluation_path = os.path.join(run_timestamp, "evaluation_data")
-os.makedirs(results_path, exist_ok = True)
-
-run_path = os.path.join('results', results_path)
-os.mkdir(run_path)
-
-executor = submitit.AutoExecutor(folder="results/our_update")
-
+executor = submitit.AutoExecutor(folder=outputpath)
 executor.update_parameters(timeout_min = 6000, mem_gb = 5, gpus_per_node = 0, cpus_per_task = 1, slurm_array_parallelism = 128)
 
 jobs = []
